4676bc1d034256849d6d69bfda334eda435203f5
teddy-hoo <hulingchuan@hotmail.com>
2014-05-23 17:06:44 +0800

    enabel resize

--- draw/draw.js
+++ draw/draw.js
@@ -9,35 +9,18 @@ var Draw = function(){
 	},
 	this.width = 960 - this.margin.left - this.margin.right,
 	this.height = 500 - this.margin.top - this.margin.bottom;
-
 	this.parseDate = d3.time.format("%Y%m%d").parse;
-
-	this.x = d3.time.scale()
-		.range([0, this.width]);
-
-	this.y = d3.scale.linear()
-		.range([this.height, 0]);
-
-	this.color = d3.scale.category10();
-
-	this.xAxis = d3.svg.axis()
-		.scale(this.x)
-		.orient("bottom");
-
-	this.yAxis = d3.svg.axis()
-		.scale(this.y)
-		.orient("left");
-
-	this.line = d3.svg.line()
-		.interpolate("basis")
-		.x(function(d) {
-			return that.x(d.time);
-		})
-		.y(function(d) {
-			return that.y(d.temperature);
-		});
 	this.svg = null;
 	this.cities = [];
+};
+
+Draw.prototype.resize = function(object){
+	if(object.width){
+		this.width = object.width - this.margin.left - this.margin.right;
+		d3.selectAll("svg").remove();
+		this.svg = null;
+		this.render();
+	}
 };
 
 Draw.prototype.setData = function(data){
@@ -48,7 +31,7 @@ Draw.prototype._createSVG = function(){
 	if(this.svg === null){
 		this.svg = d3.select("#canvas").append("svg")
 		.attr("id", "chart")
-		.attr("width", this.width + this.margin.left + this.margin.right)
+		.attr("width", this.width)
 		.attr("height", this.height + this.margin.top + this.margin.bottom)
 		.append("g")
 		.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
@@ -58,12 +41,26 @@ Draw.prototype._createSVG = function(){
 Draw.prototype._prepareData = function(){
 	var that = this;
 
+	this.x = d3.time.scale().range([0, this.width - 150]);
+	this.y = d3.scale.linear().range([this.height, 0]);
+	this.color = d3.scale.category10();
+	this.xAxis = d3.svg.axis().scale(this.x).orient("bottom");
+	this.yAxis = d3.svg.axis().scale(this.y).orient("left");
+	this.line = d3.svg.line()
+		.interpolate("basis")
+		.x(function(d) {
+			return that.x(d.time);
+		})
+		.y(function(d) {
+			return that.y(d.frequency);
+		});
+
 	this.color.domain(d3.keys(this.data[0]).filter(function(key) {
 		return key !== "time";
 	}));
 
 	this.data.forEach(function(d) {
-		d.time = that.parseDate(d.time);
+		d.time = typeof(d.time) === "string" ? that.parseDate(d.time) : d.time;
 	});
 
 	this.cities = this.color.domain().map(function(name) {
@@ -72,7 +69,7 @@ Draw.prototype._prepareData = function(){
 			values: that.data.map(function(d) {
 				return {
 					time: d.time,
-					temperature: +d[name]
+					frequency: +d[name]
 				};
 			})
 		};
@@ -85,12 +82,12 @@ Draw.prototype._prepareData = function(){
 	this.y.domain([
 		d3.min(this.cities, function(c) {
 			return d3.min(c.values, function(v) {
-				return v.temperature;
+				return v.frequency;
 			});
 		}),
 		d3.max(this.cities, function(c) {
 			return d3.max(c.values, function(v) {
-				return v.temperature;
+				return v.frequency;
 			});
 		})
 	]);
@@ -113,45 +110,20 @@ Draw.prototype.render = function(){
 		.attr("y", 6)
 		.attr("dy", ".71em")
 		.style("text-anchor", "end")
-		.text("Temperature (ºF)");
+		.text("Word Frequency");
 
-	var city = this.svg.selectAll(".city")
-		.data(this.cities)
-		.enter().append("g")
-		.attr("class", "city");
-
-	city.append("path")
-		.attr("class", "line")
-		.attr("d", function(d) {
-			return that.line(d.values);
-		})
-		.style("stroke", function(d) {
-			return that.color(d.name);
-		});
-
-	city.append("text")
-		.attr("class", "tag")
-		.datum(function(d) {
-			return {
-				name: d.name,
-				value: d.values[d.values.length - 1]
-			};
-		})
-		.attr("transform", function(d) {
-			return "translate(" + that.x(d.value.time) + "," + that.y(d.value.temperature) + ")";
-		})
-		.attr("x", 3)
-		.attr("dy", ".35em")
-		.text(function(d) {
-			return d.name;
-		});
+	this._line();
 };
 
-Draw.prototype.change = function(){
+Draw.prototype.update = function(){
 	that = this;
 	this._prepareData();
 	this.svg.selectAll(".city").remove();
+	this._line();
+};
 
+Draw.prototype._line = function(){
+	that = this;
 	var city = this.svg.selectAll(".city")
 		.data(this.cities)
 		.enter().append("g")
@@ -175,7 +147,7 @@ Draw.prototype.change = function(){
 			};
 		})
 		.attr("transform", function(d) {
-			return "translate(" + that.x(d.value.time) + "," + that.y(d.value.temperature) + ")";
+			return "translate(" + that.x(d.value.time) + "," + that.y(d.value.frequency) + ")";
 		})
 		.attr("x", 3)
 		.attr("dy", ".35em")
