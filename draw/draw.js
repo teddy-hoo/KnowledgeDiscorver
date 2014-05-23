4d20ea05f9bcc966723e0947a668c7655d0ec500
teddy-hoo <hulingchuan@hotmail.com>
2014-05-23 13:03:04 +0800

    make redraw enable

--- draw/draw.js
+++ draw/draw.js
@@ -149,23 +149,22 @@ Draw.prototype.render = function(){
 Draw.prototype.change = function(){
 	that = this;
 	this._prepareData();
+
+	this.svg.selectAll(".city").remove();
+
 	var city = this.svg.selectAll(".city")
 		.data(this.cities)
 		.enter().append("g")
-		.attr("class", "city").append("path")
+		.attr("class", "city");
+
+	city.append("path")
+		.attr("class", "line")
 		.attr("d", function(d) {
 			return that.line(d.values);
+		})
+		.style("stroke", function(d) {
+			return that.color(d.name);
 		});
-
-	// city.append("path")
-		// .transition().duration(500)
-		// .attr("class", "line")
-		// .attr("d", function(d) {
-		// 	return that.line(d.values);
-		// });
-		// .style("stroke", function(d) {
-		// 	return that.color(d.name);
-		// });
 
 	city.append("text")
 		.datum(function(d) {
@@ -182,5 +181,39 @@ Draw.prototype.change = function(){
 		.text(function(d) {
 			return d.name;
 		});
-	city.exit().remove();
+
+	// var city = this.svg.selectAll(".city")
+	// 	.data(this.cities)
+	// 	.enter().append("g")
+	// 	.attr("class", "city").append("path")
+	// 	.attr("d", function(d) {
+	// 		return that.line(d.values);
+	// 	});
+
+	// // city.append("path")
+	// 	// .transition().duration(500)
+	// 	// .attr("class", "line")
+	// 	// .attr("d", function(d) {
+	// 	// 	return that.line(d.values);
+	// 	// });
+	// 	// .style("stroke", function(d) {
+	// 	// 	return that.color(d.name);
+	// 	// });
+
+	// city.append("text")
+	// 	.datum(function(d) {
+	// 		return {
+	// 			name: d.name,
+	// 			value: d.values[d.values.length - 1]
+	// 		};
+	// 	})
+	// 	.attr("transform", function(d) {
+	// 		return "translate(" + that.x(d.value.time) + "," + that.y(d.value.temperature) + ")";
+	// 	})
+	// 	.attr("x", 3)
+	// 	.attr("dy", ".35em")
+	// 	.text(function(d) {
+	// 		return d.name;
+	// 	});
+	// city.exit().remove();
 };
