0c0be01f465f984ecc371676effcefb5ecd0291b
teddy-hoo <hulingchuan@hotmail.com>
2014-05-25 22:33:30 +0800

    add shadow box dynamic count last time

--- libs/shadowbox/shadowbox.js
+++ libs/shadowbox/shadowbox.js
@@ -1,12 +1,16 @@
 var ShadowBox = function(content){
 	this.content = content;
+	this.waittime = 3;
 	this.$shadowdiv = $('<div />');
 	this.$shadowdiv.addClass("shadowbox");
 	this.$shadowdiv.attr("id", "shadow");
+	this.$shadowdiv.text("please wait " + this.waittime.toString() + " seconds at most...");
+	this.countInterval = null;
 };
 
 ShadowBox.prototype.add = function(){
 	$(document.body).append(this.$shadowdiv);
+	this._startCount();
 	this.resize();
 };
 
@@ -19,4 +23,19 @@ ShadowBox.prototype.resize = function(){
 
 ShadowBox.prototype.remove = function(){
 	$("#shadow").remove();
+};
+
+ShadowBox.prototype._startCount = function(){
+	that = this;
+	this.countInterval = setInterval(function(){
+		that.waittime -= 0.5;
+		if(that.waittime >= 0){
+			that.$shadowdiv.text("please wait " + that.waittime.toString() + " seconds at most...");
+		}		
+	},
+	500);
+};
+
+ShadowBox.prototype.stopCount = function(){
+	clearInterval(this.countInterval);
 };
