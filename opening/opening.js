61ee70c2eddf4cdcf38167db50afdbb067d2abd9
teddy-hoo <hulingchuan@hotmail.com>
2014-06-13 20:40:37 +0800

    modify animation

--- opening/opening.js
+++ opening/opening.js
@@ -13,6 +13,13 @@ Opening.prototype.play = function(){
 		.attr("rx", 0)
 		.attr("ry", 0)
 		.attr("fill", "#FFC0CB");
+	this.svgforStroke = this.selection.append("svg")
+				.attr("id", "strokesvg")
+				.style("left", this.circle.attr("cx") - 200)
+				.style("top", this.circle.attr("cy") - 100)
+				.style("width", 800)
+				.style("height", 400)
+				.style("position", "absolute");
 	this._scaleChange();
 	this._charStroke();
 };
@@ -20,15 +27,15 @@ Opening.prototype.play = function(){
 Opening.prototype._charStroke = function(){
 	var that = this;
 	var charSData = charStrokeData();
-	var chars = ["path3076", "path3078"];
+	var chars = ["path3050"];
 	chars.forEach(function(charName){
-		that.svg.append("path")
-            .attr("d",charSData[charName])
+		that.svgforStroke.append("path")
+            .attr("d", charSData[charName].replace(/m .+? /gi, "m 100,150 "))
             .style("stroke-width", 2)
             .style("stroke", "steelblue")
             .style("fill", "none")
             .attr('id', charName);
-        that._simulatePathDrawing($("#" + charName)[0]);
+        setTimeout(function(){that._simulatePathDrawing($("#" + charName)[0]);}, 1000);
 	});
 }
 
@@ -37,39 +44,25 @@ Opening.prototype._scaleChange = function(){
 		.transition()
 		.duration(500)
 		.delay(0)
-		.attr('rx', 150)
-		.attr('ry', 150);
+		.attr('rx', 110)
+		.attr('ry', 110);
 	this.circle
 		.transition()
 		.duration(500)
-		.delay(500)
-		.attr('rx', 100)
-		.attr('ry', 100);	
-	this.circle
-		.transition()
-		.duration(500)
-		.delay(2000)
+		.delay(700)
 		.attr("fill", "#FFC0CB")
-		.attr('rx', 260)
-		.attr('ry', 160);
+		.attr('rx', 400)
+		.attr('ry', 300);
 }
 
 Opening.prototype._simulatePathDrawing = function (path) {
-  // var path = document.querySelector('.squiggle-animated path');
   var length = path.getTotalLength();
-  // Clear any previous transition
   path.style.transition = path.style.WebkitTransition =
   'none';
-  // Set up the starting positions
   path.style.strokeDasharray = length + ' ' + length;
   path.style.strokeDashoffset = length;
-  // Trigger a layout so styles are calculated & the browser
-  // picks up the starting position before animating
   path.getBoundingClientRect();
-  // Define our transition
-  path.style.transition = path.style.WebkitTransition =
-  'stroke-dashoffset 1.5s ease-in-out';
-  // Go!
+  path.style.transition = path.style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out';
   path.style.strokeDashoffset = '0';
   path.style.strokeWidth = '3px';
   path.style.fill = 'rgba(255,255,0,.12)';
