5edccaa289236d24356db699b2bf830e217dbc8d
teddy-hoo <hulingchuan@hotmail.com>
2014-06-24 17:59:02 +0800

    correct opening char position

--- opening/makeTitle.js
+++ opening/makeTitle.js
@@ -6,6 +6,8 @@ var Title = function(rawTitle, selection){
   this.container = null;
   this.titleElements = [];
   this.colorPalette = {};
+  this.fontWidth = "50px";
+  this.fontHeight = "50px";
 };
 
 Title.prototype.setSelection = function(selection){
@@ -16,7 +18,7 @@ Title.prototype.makeTtile = function(){
   that = this;
   this._createContainer();
   this.selection.appendChild(this.container);
-  this.container.setAttribute("style", '-webkit-perspective-origin: 150% 150%;-webkit-perspective: 250px;-webkit-transform-style: preserve-3d;');
+  this.container.setAttribute("style", 'height: 200px; -webkit-perspective-origin: 150% 150%;-webkit-perspective: 250px;-webkit-transform-style: preserve-3d;');
   this.titleElements.forEach(function(e){
     that.container.appendChild(e);
   });
@@ -30,12 +32,16 @@ Title.prototype._createContainer = function(){
     var letterdiv = document.createElement("div");
     var letterElement = document.createElement("span");
     letterdiv.appendChild(letterElement);
-    that.titleElements.push(letterdiv);
+    that.titleElements.push(letterElement);
     if(that.colorPalette[letter] === undefined){
       that.colorPalette[letter] = fontColor[(index++) % fontColor.length];
     }
-    letterdiv.textContent = letter;
-    letterdiv.setAttribute("style", "color:" + that.colorPalette[letter]);
-    letterdiv.setAttribute("style", 'height: 100px; wigdth: 100px;-webkit-transform: translateZ(' + index * 50 + 'px)');
+    letterElement.textContent = letter;
+    letterElement.setAttribute("style", "color:" + that.colorPalette[letter]);
+    letterElement.setAttribute("style", 'color:' + that.colorPalette[letter] + '; -webkit-transform: translateZ(' + index * 1 + 'px)');
   });
+};
+
+Title.prototype._getPerfectPosition = function(){
+  var length = this.container.getAttribute("width");
 };
