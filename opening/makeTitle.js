35b7899fd2159084192b5ed751d075f49c5b9a9a
teddy-hoo <hulingchuan@hotmail.com>
2014-06-25 20:18:50 +0800

    add title animation

--- opening/makeTitle.js
+++ opening/makeTitle.js
@@ -1,48 +1,95 @@
 var fontColor = ["#00FA9A", "#0000FF", "#FF1493", "#808080", "#FF0000", "#00FF00", "#FFD700", "#00FFFF"];
 
-var Title = function(rawTitle, selection){
+var Title = function(rawTitle, selection, fontColors, width, height, fontsize){
   this.rawTitle = rawTitle.split("");
   this.selection = selection;
   this.container = null;
+  this.fontSize = fontsize;
   this.titleElements = [];
   this.colorPalette = {};
+  this.fontColors = fontColors || fontColor;
   this.fontWidth = "50px";
   this.fontHeight = "50px";
+  this.letterPerLine = 0;
+  this.lineCount = 1;
+  this.containerWidth = width || window.innerWidth;
+  this.containerHeight = height || 100;
 };
 
 Title.prototype.setSelection = function(selection){
   this.selection = selection;
 };
 
+Title.prototype.resize = function(){
+  //finish later
+};
+
 Title.prototype.makeTtile = function(){
-  that = this;
+  var that = this;
+  this.container = document.createElement("div");
+  this.selection.setAttribute("style", "width: " + this.containerWidth +
+                              "px; height: " + this.containerHeight + "px;");
   this._createContainer();
   this.selection.appendChild(this.container);
-  this.container.setAttribute("style", 'height: 100px; -webkit-perspective-origin: 150% 150%;-webkit-perspective: 250px;-webkit-transform-style: preserve-3d;');
-  this.titleElements.forEach(function(e){
-    that.container.appendChild(e);
-  });
 };
 
 Title.prototype._createContainer = function(){
-  that = this;
-  this.container = document.createElement("div");
-  var index = 0;
+  this._getPerfectPosition();
+  this.container.setAttribute("style", "width: " + this.containerWidth +
+                              "px; height: " + this.containerHeight +
+                              "px; font-size: " + this.fontSize + "px;");
+
+  var that = this;
   this.rawTitle.forEach(function(letter, index){
     var letterdiv = document.createElement("div");
-    var letterElement = document.createElement("span");
-    letterdiv.appendChild(letterElement);
-    that.titleElements.push(letterElement);
+    that.titleElements.push(letterdiv);
     if(that.colorPalette[letter] === undefined){
-      that.colorPalette[letter] = fontColor[(index++) % fontColor.length];
+      that.colorPalette[letter] = that.fontColors[(index) % that.fontColors.length];
     }
-    letterElement.textContent = letter;
-    letterElement.setAttribute("style", "color:" + that.colorPalette[letter]);
-    letterElement.setAttribute("style", 'color:' + that.colorPalette[letter]
-                               + '; -webkit-transform: translateZ(' + index * 1 + 'px)');
+    letterdiv.textContent = letter;
+    letterdiv.setAttribute("id", "font" + index);
+    letterdiv.setAttribute("style", 'background-color: #FFE4E1; position: absolute;' +
+                           'color:' + that.colorPalette[letter] +
+                           '; width: 0px; height: 0px; top: ' + (parseInt(index / that.letterPerLine) * that.fontHeight) +
+                           'px; left: ' + (parseInt(index % that.letterPerLine) * that.fontWidth) + 'px;' +
+                           'transition-duration: 1s; text-align: center; vertical-align: middle;' +
+                           '-webkit-transition:height 1s, width 1s;');
   });
+  this.titleElements.forEach(function(e){
+    that.container.appendChild(e);
+  });
+  setTimeout(function(){
+    var nodes = that.container.childNodes;
+  for(var key in nodes){
+    if(nodes[key].style && nodes[key].style){
+      nodes[key].style.width = that.fontWidth - 10 + "px";
+      nodes[key].style.height = that.fontHeight - 10 + "px";
+      //nodes[key].style.line-height = that.fontHeight + "px";
+    }
+  }
+  }, 3500);
+  //var nodes = this.container.childNodes;
+  //for(var key in nodes){
+  //  if(nodes[key].style && nodes[key].style){
+  //    nodes[key].style.width = this.fontWidth + "px";
+  //    nodes[key].style.height = this.fontHeight + "px";
+  //  }
+  //}
 };
 
 Title.prototype._getPerfectPosition = function(){
-  var length = this.container.getAttribute("width");
+  var letterCount = this.rawTitle.length;
+
+  if(parseInt(this.containerWidth / letterCount) >= 20){
+    this.fontWidth = parseInt(this.containerWidth / letterCount);
+    this.fontHeight = parseInt(this.containerHeight);
+    this.letterPerLine = letterCount;
+  }
+  else{
+    this.letterPerLine = parseInt(this.containerWidth / 20);
+    this.lineCount = Math.ceil(letterCount / this.letterPerLine);
+    this.fontWidth = parseInt(this.containerWidth / letterCount);
+    this.fontHeigth = parseInt(this.containerHeight / this.lineCount);
+  }
+  this.fontSize = this.fontSize || (this.fontWidth > this.fontHeight ? this.fontHeight : this.fontWidth) + 10;
 };
